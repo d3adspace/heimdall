6aa50060ccd984e53cd8e41fa5fd420653207202
Felix Klauke <info@felix-klauke.de>
2019-04-25 08:50:13 +0200

    Add first simple server bootstrap and corresponding maven jar plugin.

---
+++ server/src/main/java/de/d3adspace/heimdall/server/HeimdallServerBootstrap.java
@@ -0,0 +1,18 @@
+package de.d3adspace.heimdall.server;
+
+import de.d3adspace.heimdall.server.config.HeimdallServerConfig;
+import de.d3adspace.heimdall.server.config.HeimdallServerConfigBuilder;
+
+public class HeimdallServerBootstrap {
+
+    public static void main(String[] args) {
+
+        HeimdallServerConfig config = new HeimdallServerConfigBuilder()
+                .setServerHost("localhost")
+                .setServerPort(1337)
+                .createHeimdallServerConfig();
+
+        HeimdallServer heimdallServer = HeimdallServerFactory.createHeimdallServer(config);
+        heimdallServer.start();
+    }
+}
